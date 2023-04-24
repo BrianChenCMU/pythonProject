376e39eebbe89f06cc5ba0eff095f0b1d9c489ac
BrianChenCMU <123510548+BrianChenCMU@users.noreply.github.com>
2023-04-24 14:19:57 -0400

    Add files via upload

---
+++ pkg/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='s23openalex',
+      version='0.0.1',
+      description='bibtex utilities',
+      maintainer='Po Chun Chen',
+      maintainer_email='pochunc@andrew.cmu.edu',
+      license='CMU',
+      packages=['s23openalex'],
+      scripts=[],
+      long_description='''A set of bibtex utilities''')
